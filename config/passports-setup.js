1419298bdc774ca16c34af1c31d94fbf8c334e06
stevens <stevensqiu@gmail.com>
2020-07-29 17:09:09 -0700

    google oauth

--- config/passports-setup.js
+++ config/passports-setup.js
@@ -1,43 +1,81 @@
+
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const localStrategy = require("passport-local").Strategy;
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20');
-const User = require('../models/User');
-const gooogleClientID = process.env.clientID;
-const googleClientSecret = process.env.clientSecret; 
-
-passport.serializeUser((user, done)=> {
-    done(null, user.id);
-});
-
-passport.deserializeUser((id, done)=> {
-    User.findById(id).then((user) => {
-        done(null, user);
+const { google } = require("googleapis");
+// module.exports = function (passport) {
+  
+  passport.serializeUser((user, cb) => {
+    cb(null, user);
+  });
+  
+  passport.deserializeUser((id, cb) => {
+    User.findOne({ _id: id }, (err, user) => {
+      cb(err, user);
     });
-});
+  });
 
-
-passport.use(
-    new GoogleStrategy({
-        // options for google strategy
-        callbackURL:'/auth/google/redirect',
-        clientID: gooogleClientID,
-        clientSecret: googleClientSecret
-    }, (accessToken, refreshToken, profile, done) => {
-        // check if user already exists in our db
-        User.findOne({googleId: profile.id}).then((currentUser)=> {
-            if (currentUser) {
-               // already have the user registered 
-               console.log('already have this user');
-               done(null, currentUser);
-            } else {
-                // create a new user
-                new User({
-                    username: profile.displayName,
-                    googleId: profile.id
-                }).save().then((newUser) => {
-                    console.log('new user created: ' + newUser);
-                    done(null, newUser);
-                });
-            }
-        })
+  passport.use(
+    new localStrategy((username, password, done) => {
+      User.findOne({ username: username }, (err, user) => {
+        if (err) throw err;
+        if (!user) return done(null, false);
+        bcrypt.compare(password, user.password, (err, result) => {
+          if (err) throw err;
+          if (result === true) {
+            return done(null, user);
+          } else {
+            return done(null, false);
+          }
+        });
+      });
     })
-)
+  );
+
+  passport.use(
+      new GoogleStrategy({
+          // options for google strategy
+          callbackURL: '/auth/google/redirect',
+          clientID: process.env.Google_clientID,
+          clientSecret: process.env.Google_clientSecret
+      }, (accessToken, refreshToken, profile, done) => {
+          // check if user already exists in our db
+          User.findOne({ googleId: profile.id }).then((currentUser) => {
+              if (currentUser) {
+                  // already have the user registered 
+                  console.log('already have this user');
+                  done(null, currentUser);
+              } else {
+                  // create a new user
+                  new User({
+                      username: profile.displayName,
+                      googleId: profile.id
+                  }).save().then((newUser) => {
+                      console.log('new user created: ' + newUser);
+                      done(null, newUser);
+                  });
+              }
+          })
+      })
+  )
+  
+  // passport.use(
+  //     new FacebookStrategy({
+  //         clientID: process.env.Facebook_clientID,
+  //         clientSecret: process.env.Facebook_clientSecret,
+  //         callbackURL: '/auth/facebook/redirect'
+  //     }, (accessToken, refreshToken, profile, cb) => {
+  //       User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+  //         return done(err, user);
+  //       });
+  //     })
+  // );
+  
+
+// };
+
+
+
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,12 @@
 const router = require('express').Router();
 const passport = require('passport');
+const bcrypt = require("bcryptjs");
+const User = require('../models/User');
 
-// auth login
-router.get('/login', (req, res) => {
-    res.render('login');
-});
-
-// auth logout
-router.get('/logout', (req, res) => {
-    // handle with passport
-   req.logout();
-   res.redirect('/');
+// get current auth user
+router.get('/user', (req, res) => {
+    console.log(req.user);
+    res.send(req.user);
 });
 
 // auth with google
@@ -22,8 +18,68 @@ router.get('/google', passport.authenticate('google', {
 
 // callback route for google to redirect to
 router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
-    res.send(req.body);
+    res.send(req.user);
 });
 
+router.get("/logout", (req, res, next) => {
+    req.logOut();
+    res.send("Logout successful");
+});
+
+// router.post("/login", (req, res, next) => {
+//     passport.authenticate("local", (err, user, info) => {
+//         if (err) throw err;
+//         if (!user) res.send("No User Exists");
+//         else {
+//             req.logIn(user, (err) => {
+//                 if (err) throw err;
+//                 res.send("Successfully Authenticated");
+//             });
+//         }
+//     })(req, res, next);
+// });
+
+
+// router.post('/register', (req, res) => {
+//     User.findOne({ username: req.body.username }, async (err, doc) => {
+//         if (err) throw err;
+//         if (doc) res.send("User already exists");
+//         if (!doc) {
+//             const hashedPassword = await bcrypt.hash(req.body.password, 10);
+//             const newUser = new User({
+//                 username: req.body.username,
+//                 password: hashedPassword
+//             });
+//             await newUser.save();
+//             res.send('User created');
+//         }
+//     })
+// });
+
+
+
+// // auth with google
+// router.get('/google',
+//     passport.authenticate('google', {
+//         scope: ['profile', 'email']
+//     })
+// );
+
+// router.get('/google/redirect',
+//     passport.authenticate('google'),
+//     (req, res) => {
+//         res.redirect('http://localhost:3000');
+//     });
+
+// router.get('/facebook',
+//     passport.authenticate('facebook')
+// );
+
+// router.get('/facebook/redirect',
+//     passport.authenticate('facebook'),
+//     (req, res) => {
+//         res.redirect('http://localhost:3000/profile');
+//     }
+// );
 
 module.exports = router;
